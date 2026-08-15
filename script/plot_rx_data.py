f6cf4b33a4fa30e8714067e117fab22faa39e6f7
MuhammadYousaf79 <yousafimran119@gmail.com>
2026-08-15 21:49:42 +0500

    improved output hex format

--- script/plot_rx_data.py
+++ script/plot_rx_data.py
@@ -6,14 +6,20 @@ rx_file = "script/rx_file.csv"
 
 
 with open(rx_file, "w") as f:
+    f.write("0x")
     while True:
         for i in range(4):
             data = ser.read()
             if data:
-                f.write(hex(ord(data)))
-        f.write(",")
+                f.write(f"{data[0]:02X}")
+                # if (i == 0): 
+                #     f.write(hex(ord(data)))
+                # else:
+                #     f.write(hex(ord(data))[2:])
+        f.write(",0x")
         for i in range(4):
             data = ser.read()
             if data:
-                f.write(hex(ord(data)))
-        f.write("\n")
+                # f.write(hex(ord(data))[2:])
+                f.write(f"{data[0]:02X}")
+        f.write("\n0x")
